482a1197cbf0a6b397fedfa1a7daa37e406374ac
Kirti Kunj Bajpai <70293103+kkb-10@users.noreply.github.com>
2021-06-26 11:35:12 +0530

    Add files via upload
    Added starting page

--- web-scrapping.py
+++ web-scrapping.py
@@ -7,9 +7,10 @@ import os
 # Provide the below details for your codeforces profile
 
 cf_handle=""                   # Your cf handle
-no_of_pages=10                 # No of submission pages in your profile
+no_of_pages=12                # No of submission pages in your profile
 path="D:\code\Code-Library\ "  # Path where you want to save all AC submssion in your local repo
-partymemberid="9614744"        # Inspect element any submission id from your profile and get your partymemberid from there
+partymemberid="9214927"        # Inspect element any submission id from your profile and get your partymemberid from there
+start_page= 1                  # The submission page number from where you want to start scraping the solutions
   
 ############# NOW THE MAGIC BEGINS - BY Kirti Kunj Bajpai ###############
 
@@ -19,7 +20,7 @@ print("Scrapping the AC Solutions of "+cf_handle + "...........", end="\n\n\n")
 
 count =1
 
-for i in range (1,no_of_pages+1):
+for i in range (start_page,no_of_pages+1):
     print("Scrapping Page " + str(i) + "......")
     print("***********************************",end="\n\n")
     req_url =url + str(i)
