8f5feacc0de9b824179b154f3f9dacfdec76d42d
Kirti Kunj Bajpai <70293103+kkb-10@users.noreply.github.com>
2021-06-26 11:25:53 +0530

    Add files via upload
    Handled invalid file name cases

--- web-scrapping.py
+++ web-scrapping.py
@@ -6,10 +6,10 @@ import os
 
 # Provide the below details for your codeforces profile
 
-cf_handle="kkb_10"              # Your cf handle
-no_of_pages=10                  # No of submission pages in your profile
-path="D:\code\Code-Library\ "   # Path where you want to save all AC submssion in your local repo
-partymemberid="934927"          # Inspect element any submission id from your profile and get your partymemberid from there
+cf_handle=""                   # Your cf handle
+no_of_pages=10                 # No of submission pages in your profile
+path="D:\code\Code-Library\ "  # Path where you want to save all AC submssion in your local repo
+partymemberid="9614744"        # Inspect element any submission id from your profile and get your partymemberid from there
   
 ############# NOW THE MAGIC BEGINS - BY Kirti Kunj Bajpai ###############
 
@@ -17,10 +17,13 @@ url="https://codeforces.com/submissions/" + cf_handle + "/page/"
 
 print("Scrapping the AC Solutions of "+cf_handle + "...........", end="\n\n\n")
 
-for i in range (1,no_of_pages+1):
+count =1
+
+for i in range (3,no_of_pages+1):
     print("Scrapping Page " + str(i) + "......")
     print("***********************************",end="\n\n")
     req_url =url + str(i)
+
     html_content = requests.get(req_url).text  
     
     soup = BeautifulSoup(html_content, "html5lib")  
@@ -53,7 +56,8 @@ for i in range (1,no_of_pages+1):
 
             # Storing into the local repo
 
-            final_path = path + problem_name +".txt"
+            final_path = path + "problem" + str(count) + " - code.txt"
+            
             req_path = path + problem_name + "." + subm_type
             if not os.path.exists(path + problem_name + "." + subm_type): # If that file name already exists, then it doesn't add it to repo
                 file = open(final_path, "w+") 
@@ -66,5 +70,6 @@ for i in range (1,no_of_pages+1):
                 os.rename(final_path, base + ext)
 
             print(problem_name)
-            print("Saving file at: " + path + problem_name + "." + subm_type ,end="\n\n")
+            print("Saving file at: " + path + "problem" + str(count) + " - code." + subm_type ,end="\n\n")
+            count+=1
 
