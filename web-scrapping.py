cdc8d7182f447f44cf42003ea3a671f86fce2e35
Kirti Kunj Bajpai <70293103+kkb-10@users.noreply.github.com>
2021-06-26 12:33:35 +0530

    Add files via upload
    Handled file name error

--- web-scrapping.py
+++ web-scrapping.py
@@ -7,9 +7,9 @@ import os
 # Provide the below details for your codeforces profile
 
 cf_handle=""                   # Your cf handle
-no_of_pages=12                # No of submission pages in your profile
+no_of_pages=10                 # No of submission pages in your profile
 path="D:\code\Code-Library\ "  # Path where you want to save all AC submssion in your local repo
-partymemberid="9214927"        # Inspect element any submission id from your profile and get your partymemberid from there
+partymemberid="9614927"        # Inspect element any submission id from your profile and get your partymemberid from there
 start_page= 1                  # The submission page number from where you want to start scraping the solutions
   
 ############# NOW THE MAGIC BEGINS - BY Kirti Kunj Bajpai ###############
@@ -18,7 +18,6 @@ url="https://codeforces.com/submissions/" + cf_handle + "/page/"
 
 print("Scrapping the AC Solutions of "+cf_handle + "...........", end="\n\n\n")
 
-count =1
 
 for i in range (start_page,no_of_pages+1):
     print("Scrapping Page " + str(i) + "......")
@@ -57,10 +56,9 @@ for i in range (start_page,no_of_pages+1):
 
             # Storing into the local repo
 
-            final_path = path + "problem" + str(count) + " - code.txt"
+            final_path = path + "problem - " + submission_id  + " - code.txt"
             
-            req_path = path + problem_name + "." + subm_type
-            if not os.path.exists(path + problem_name + "." + subm_type): # If that file name already exists, then it doesn't add it to repo
+            if not os.path.exists(path + "problem - " + submission_id + " - code." + subm_type): # If that file name already exists, then it doesn't add it to repo
                 file = open(final_path, "w+") 
                 file.write(code)
                 file.close()
@@ -71,6 +69,5 @@ for i in range (start_page,no_of_pages+1):
                 os.rename(final_path, base + ext)
 
             print(problem_name)
-            print("Saving file at: " + path + "problem" + str(count) + " - code." + subm_type ,end="\n\n")
-            count+=1
+            print("Saving file at: " + path + "problem - " + submission_id + " - code." + subm_type ,end="\n\n")
 
